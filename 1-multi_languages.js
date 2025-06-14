8ae5c3ebfb89c9716b1c3d3e56a35c59e37f353c
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-06-14 21:42:53 +0300

    Add script to print 3 lines using console.log

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,1 +1,8 @@
-console.log("C is fun");   console.log("Python is cool");      console.log("JavaScriptis amazing");
+
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
